1593ffb590fff2b0ab7e3d5121db324d1088b68e
Martin <tmartin193@gmail.com>
2018-03-18 19:55:40 +0100

    Add delay with mono

--- src/main/java/com/martin/webflux/WebController.java
+++ src/main/java/com/martin/webflux/WebController.java
@@ -1,29 +1,22 @@
 package com.martin.webflux;
 
 import com.codahale.metrics.annotation.Metered;
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.martin.webflux.model.Video;
-import io.vertx.core.Vertx;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class WebController
 {
-    private final ScheduledThreadPoolExecutor threadPoolExecutor;
-    private final Vertx vertx;
     private final AtomicInteger atomicInteger;
 
     public WebController()
     {
-        threadPoolExecutor = new ScheduledThreadPoolExecutor(100, new ThreadFactoryBuilder().setNameFormat("my-scheduler-pool-%d").build());
-        vertx = Vertx.vertx();
         atomicInteger = new AtomicInteger(1);
     }
 
@@ -48,13 +41,14 @@ public class WebController
     @GetMapping("/slow")
     public Mono<String> slow()
     {
-        return Mono.<String>create(stringMonoSink -> threadPoolExecutor.schedule(() ->
-                stringMonoSink.success(atomicInteger.getAndIncrement() + ": Slow response... on remote server thread: "
-                        + Thread.currentThread().getName() + " " + LocalDateTime.now()), 3, TimeUnit.SECONDS))
-                .doOnSuccess(System.out::println);
+//        return Mono.<String>create(stringMonoSink -> threadPoolExecutor.schedule(() ->
+//                stringMonoSink.success(atomicInteger.getAndIncrement() + ": Slow response... on remote server thread: "
+//                        + Thread.currentThread().getName() + " " + LocalDateTime.now()), 3, TimeUnit.SECONDS))
+//                .doOnSuccess(System.out::println);
 
-//        return Mono.delay(Duration.ofMillis(3000))
-//                   .flatMap(time -> Mono.just("Slow response... on remote server thread: " + Thread.currentThread().getName()));
+        return Mono.delay(Duration.ofMillis(3000))
+                   .then(Mono.just(atomicInteger.getAndIncrement() + ": Slow response... on remote server thread: "
+                           + Thread.currentThread().getName() + " " + LocalDateTime.now()));
 
 //        return Mono.create(monosink -> vertx.setTimer(
 //                3000L,
